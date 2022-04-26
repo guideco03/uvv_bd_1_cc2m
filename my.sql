caf2b4da7c2464397b480f35dfa08aefb3c1cb11
Guilherme Gobbi Decottignies <103463433+guideco03@users.noreply.github.com>
2022-04-26 19:52:24 -0300

    Update my.sql

--- my.sql
+++ my.sql
@@ -132,4 +132,45 @@ NOT DEFERRABLE;
 /* Com todas as tabelas prontas, ssuas devidas restrições e relaconamentos, agora é hora de inserir os dados na tabela de acordo com o PDF*/
 /* Dferente do PostgreSQL, aqui optei por enxutar os códigos ao invés de colocar um monte de INSERT_INTO.*/
 
+INSERT INTO Funcionario (Primeiro_Nome, Nome_Meio, Ultimo_Nome, CPF, Data_Nascimento, Enedeco, Sexo, Salario, CPF_Supervisor, Numero_Departamento)
+VALUES ('João', 'B', 'Silva', 12345678966, '09-01-1965', 'Rua das Flores, 751, São Paulo, SP', 'M', 30000, 33344555587, 5), 
+('Fernando', 'T', 'Wong', 33344555587, '08-12-1955', 'Rua da Lapa, 34, São Paulo, SP', 'M', 40000, 88866555576, 5),
+('Alice', 'J', 'Zelaya', 99988777767, '19-01-1968', 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, 98765432168, 4),
+('Jennifer', 'S', 'Souza', 98765432168, '20-06-1941', 'Av. Arthur de Lima, 54, Santo André, SP', 'F', 43000, 88866555576, 4),
+('Ronaldo', 'K', 'Lima', 66688444476, '15-09-1962', 'Rua Rebouças, 65, Piracicaba, SP', 'M', 38000, 33344555587, 5),
+('Joice', 'A', 'Leite', 45345345376, '31-07-1972', 'Av. Lucas Obes, 74, São Paulo, SP', 'F', 25000, 33344555587, 5),
+('André', 'V', 'Pereira', 98798798733, '29-03-1969', 'Rua Timbira, 35, São Paulo, SP', 'M', 25000, 98765432168, 4),
+('Jorge', 'E', 'Brito', 88866555576, '10-11-1937', 'Rua do Horto, 35, São Paulo, SP', 'M', 38000, 5);
 
+/* Bem mais enxuto né. Feito isso, foram inseridos os dados da tabela departamento */
+
+INSERT INTO Departamento (Nome_Departamento, Numero_Departamento, CPF_Gerente, Data_Inicio_Gerente)
+VALUES ('Pesquisa', 5, 33344555587, '22-05-1988'), ('Administração', 4, 98765432168, '01-01-1995'), ('Matriz', 1, 88866555576, '19-06-1981');
+
+/* Hora de inserir os dados da tabela Localizacoes_Departamento */
+
+INSERT INTO Localizacoes_Departamento (Numero_Departamento, Local)
+VALUES (1, 'São Paulo'), (4, 'Mauá'), (5, 'Santo André'), (5, 'Itu'), (5, 'São Paulo');
+
+/* Continuando inserindo mais dados... agora os da tabela Projeto */
+
+INSERT INTO Projeto (Nome_Projeto, Numero_Projeto, Local_Projeto, Numero_Departamento)
+VALUES ('ProdutoX', 1, 'Santo André', 5), ('ProdutoY', 2, 'Itu', 5), ('ProdutoZ', 3, 'São Paulo', 5), ('Informatização', 10, 'Mauá', 4),
+('Reorgnização', 20, 'São Paulo', 1), ('NovosBenefícios', 30, 'Mauá', 4);
+
+/* Colocando os dados da tabela Dependente */
+
+INSERT INTO Dependente (CPF_Funcionario, Nome_Dependente, Sexo, Data_Nascimento, Parentesco)
+VALUES (33344555587, 'Alicia', 'F', '05-04-1986', 'Filha'), (33344555587, 'Tiago', 'M', '25-10-1983', 'Filho'), (33344555587, 'Janaina', 'F', '03-05-1958', 'Esposa'),
+(98765432168, 'Antonio', 'M', '28-02-1942', 'Marido'), (12345678966, 'Michael', 'M', '04-01-1988', 'Filho'), (12345678966, 'Alicia', 'F', '30-12-1988', 'Filha'),
+(12345678966, 'Elizabeth', 'F', '05-05-1967', 'Esposa');
+
+/* E por fim, os dados da tabela Trabalha_Em. */
+
+INSERT INTO trabalha_em (cpf_funcionario, numero_projeto, horas) 
+VALUES (12345678966, 1, 32.5), (12345678966, 2, 7.5), (66688444476, 3, 40.0), (45345345376, 1, 20.0), (45345345376, 2, 20.0),
+(33344555587, 2, 10.0), (33344555587, 3, 10.0), (33344555587, 10, 10.0), (33344555587, 20, 10.0), (99988777767, 30, 30.0),
+(99988777767, 10, 10.0), (98798798733, 10, 35.0), (98798798733, 30, 5.0), (98765432168, 30, 20.0), (98765432168, 20, 15.0),
+(88866555576, 20);
+
+/*Pronto. Projeto finalizado!. */
